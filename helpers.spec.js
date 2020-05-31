3b745ebf0ecffdef4ad60b9c51918a170c842c88
marlendo <erikmarlendo@gmail.com>
2020-05-31 11:25:00 +0700

    update spec

--- helpers.spec.js
+++ helpers.spec.js
@@ -2,10 +2,12 @@ import { flickrApi } from './src/service/api';
 
 describe('Test Public API from flickr', () => {
   let data;
-  beforeEach(async () => {    
+  beforeEach(async () => {
     data = await flickrApi('public')
   });
   it('corrected format API', async () => {
+    expect(data.title != undefined).toEqual(true);
+    expect(data.items != undefined).toEqual(true);
     expect(data.items.length >= 1).toEqual(true);
   });
 });
